c1d2f0906b2b637ff5c904037af121aefcb6ad92
richard-ui <richard-jones96@hotmail.co.uk>
2021-02-28 16:58:42 +0000

    Add javascript functionality to add multiple ingredients for one field within the recipes database table

---
+++ static/js/recipe.js
@@ -0,0 +1,20 @@
+let counter = 2;
+let limit = 30;
+const newIngredient = '<div id="ingredientholder*" class="input-field col s12"><i class="fas fa-carrot prefix"></i><input id="ingredients*" type="text" name="ingredients" minlength="3" maxlength="60" class="validate" required><label for="ingredients*" placeholder="Ingredient">Ingredient *</label><a class="waves-effect waves-light btn" onClick="deleteIngredient(this)" data-ingredient="ingredientholder*"><i class="fas fa-times"></i> Remove</a></div>'
+
+function deleteIngredient(el) {
+    console.log(el.getAttribute("data-ingredient"));
+    document.getElementById(el.getAttribute("data-ingredient")).remove();
+}
+function addIngredient(divName) {
+    if (counter === limit) {
+        alert("You have reached the limit of adding " + counter + " ingredients");
+    }
+    else {
+        console.log("Adding Ingredient")
+        let newdiv = document.createElement('div');
+        newdiv.innerHTML = newIngredient.replaceAll("*", counter);
+        document.getElementById(divName).appendChild(newdiv);
+        counter++;
+    }
+};
