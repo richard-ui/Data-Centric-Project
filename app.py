589f9b9239746b5d0bac5cdad0f2e8d54d8c7c8e
richard-ui <richard-jones96@hotmail.co.uk>
2021-03-09 12:11:58 +0000

    Implement image upload method that uploads to the database and can be retrieved on recipes page used from our static images folder

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 if os.path.exists("env.py"):
     import env
 
+
+UPLOAD_FOLDER = 'static/img/'
 
 app = Flask(__name__)
 
@@ -15,14 +18,106 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 mongo = PyMongo(app)
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_file():
+    # Output will be blank if user_file key not found on submission
+    output = ""
+
+    if "user_file" not in request.files:
+
+        return output
+
+    # If the key is in the object, save it in file variable
+    file = request.files["user_file"]
+
+    # Check the filename, if it's blank, leave it blank
+    if file.filename == "":
+
+        return output
+
+    # Check that there is a file and that it has an allowed filetype
+    if file:
+        file.filename = secure_filename(file.filename)
+
+    return output
+
+
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        # return render_template('recipes.html', filename=filename)
+        return filename
+
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        # return redirect(request.url)
 
 
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
+    for recipe in recipes:
+        try:
+            recipe["user_id"] = mongo.db.users.find_one(
+                {"_id": recipe["user_id"]})["username"]
+        except:
+            pass
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/upload_form")
+def upload_form():
+    return render_template("upload.html")
+
+
+@app.route('/upload_form', methods=['POST'])
+def upload_image1():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('upload.html', filename=filename)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    # print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='img/' + filename), code=301)
 
 
 @app.route("/search_recipe", methods=["GET", "POST"])
@@ -101,6 +196,7 @@ def profile(username):
     if session["user"]:
         for recipe in recipes:
             try:
+                # find user that matches current session
                 recipe["user_id"] = mongo.db.users.find_one(
                     {"_id": recipe["user_id"]})["username"]
             except:
@@ -114,15 +210,22 @@ def profile(username):
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
+        # image_file = request.files['user_file']
+        # image_file.save(secure_filename(image_file.filename))
+        # image_file.filename
+
+        user = mongo.db.users.find_one({"username": session["user"]})
         new_recipe = {
             "recipe_name": request.form.get("recipe_name"),
             "ingredients": request.form.getlist("ingredients"),
             "prep_time": request.form.get("prep_time"),
+            "file": upload_image(),
             "prep_steps": request.form.getlist("prep_steps"),
             "cook_time": request.form.get("cook_time"),
             "cuisine_name": request.form.get("cuisine_name"),
-            "created_by": session["user"]
-        }
+            "created_by": session["user"],
+            "user_id": ObjectId(user["_id"])
+            }
         mongo.db.recipes.insert_one(new_recipe)
         flash("Recipe Successfully Added!")
         return redirect(url_for("get_recipes"))
@@ -134,6 +237,7 @@ def add_recipe():
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     if request.method == "POST":
+        user = mongo.db.users.find_one({"username": session["user"]})
         update_recipe = {
             "recipe_name": request.form.get("recipe_name"),
             "ingredients": request.form.getlist("ingredients"),
@@ -141,7 +245,8 @@ def edit_recipe(recipe_id):
             "prep_steps": request.form.getlist("prep_steps"),
             "cook_time": request.form.get("cook_time"),
             "cuisine_name": request.form.get("cuisine_name"),
-            "created_by": session["user"]
+            "created_by": session["user"],
+            "user_id": ObjectId(user["_id"])
         }
 
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, update_recipe)
